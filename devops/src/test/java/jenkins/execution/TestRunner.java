7621efae290d0ec79ad98e4ff71550acd67d7b99
Mayank Joshi <mayank.joshi2@soprasteria.com>
2022-08-27 20:29:46 +0530

    Added a SimpleDateFormat to get real time of file creation

--- devops/src/test/java/jenkins/execution/TestRunner.java
+++ devops/src/test/java/jenkins/execution/TestRunner.java
@@ -2,6 +2,9 @@ package jenkins.execution;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class TestRunner {
 
@@ -13,8 +16,9 @@ public class TestRunner {
             // create a new file with name specified
             // by the file object
             boolean value = file.createNewFile();
+            String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
             if (value) {
-                System.out.println("New Java File is created by Mayank.");
+                System.out.println("New Java File is created by Mayank at "+ timestamp);
             }
             else {
                 System.out.println("The file already exists.");
